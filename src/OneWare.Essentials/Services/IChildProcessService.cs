61f83770d57ce438f387d2177a134802a11d26d1
Hendrik Mennen <hmennen@protop-solutions.com>
2024-03-08 10:52:22 +0100

    add timer to childprocess service

--- src/OneWare.Essentials/Services/IChildProcessService.cs
+++ src/OneWare.Essentials/Services/IChildProcessService.cs
@@ -5,5 +5,6 @@ namespace OneWare.Essentials.Services;
 public interface IChildProcessService
 {
     public Task<(bool success, string output)> ExecuteShellAsync(string path, string arguments, string workingDirectory,
-        string status, AppState state = AppState.Loading);
+        string status, AppState state = AppState.Loading, bool showTimer = false, Action<string>? outputAction = null,
+        Func<string, bool>? errorAction = null);
 }
