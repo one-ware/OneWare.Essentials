01640b1a0db38096f1a69c9cebf1a117614b63e2
Hendrik Mennen <hmennen@protop-solutions.com>
2024-03-12 21:32:22 +0100

    update childprocessservice

--- src/OneWare.Essentials/Services/IChildProcessService.cs
+++ src/OneWare.Essentials/Services/IChildProcessService.cs
@@ -5,6 +5,6 @@ namespace OneWare.Essentials.Services;
 public interface IChildProcessService
 {
     public Task<(bool success, string output)> ExecuteShellAsync(string path, IReadOnlyCollection<string> arguments, string workingDirectory,
-        string status, AppState state = AppState.Loading, bool showTimer = false, Action<string>? outputAction = null,
+        string status, AppState state = AppState.Loading, bool showTimer = false, Func<string, bool>? outputAction = null,
         Func<string, bool>? errorAction = null);
 }
