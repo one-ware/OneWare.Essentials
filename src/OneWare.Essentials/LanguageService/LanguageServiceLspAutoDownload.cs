d1164c2874660b5e18a167abf66bdefbda5b2761
Hendrik Mennen <hmennen@protop-solutions.com>
2024-03-19 02:42:50 +0100

    add autodownload setting

--- src/OneWare.Essentials/LanguageService/LanguageServiceLspAutoDownload.cs
+++ src/OneWare.Essentials/LanguageService/LanguageServiceLspAutoDownload.cs
@@ -9,14 +9,19 @@ namespace OneWare.Essentials.LanguageService;
 public abstract class LanguageServiceLspAutoDownload : LanguageServiceLsp
 {
     private readonly IPackageService _packageService;
-    private Package _package;
+    private readonly Package _package;
+    private bool _enableAutoDownload = false;
     
-    protected LanguageServiceLspAutoDownload(IObservable<string> executablePath, Package package, string name, string? workspace, IPackageService packageService) 
+    protected LanguageServiceLspAutoDownload(IObservable<string> executablePath, Package package, string name, string? workspace, IPackageService packageService, IObservable<bool> enableAutoDownload) 
         : base(name, workspace)
     {
         _package = package;
         _packageService = packageService;
-        
+
+        enableAutoDownload.Subscribe(x =>
+        {
+            _enableAutoDownload = x;
+        });
         executablePath.Subscribe(x =>
         {
             ExecutablePath = x;
@@ -27,6 +32,7 @@ public abstract class LanguageServiceLspAutoDownload : LanguageServiceLsp
     {
         if (_packageService.Packages.TryGetValue(_package.Id!, out var model) && model is {Status: PackageStatus.Available or PackageStatus.UpdateAvailable})
         {
+            if (!_enableAutoDownload) return;
             if(!await _packageService.InstallAsync(_package)) return;
         }
         await base.ActivateAsync();
